66f94ac39e57a33a3f7ec76112785d4a4d899cab
Soha Jin <soha@lohu.info>
2019-03-19 20:44:51 -0700

    allow access with numeric slug

--- src/routes/post.rs
+++ src/routes/post.rs
@@ -14,7 +14,10 @@ use super::error::Error;
 #[get("/post/<path>")]
 pub fn post_show(db: State<Database>,global_var: render::GlobalVariable,path: String)->Result<Template,Error>{
 	let slug=path.replace(".html",""); // TODO: We just need to remove `.html` at the end
-	let post:content::Content=content::Content::find_by_slug(&db,&slug)?;
+	let post=match slug.parse::<i32>(){
+		Ok(post_id)=>content::Content::find(&db,post_id)?,
+		Err(_)=>content::Content::find_by_slug(&db,&slug)?
+	};
 	if post.status==content::ContentStatus::Deleted || post.r#type!=content::ContentType::Article{
 		return Err(Error::NotFound)
 	}
