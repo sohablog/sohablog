2bcff3955d76d0c1f49634ed57719f2bf6c7152d
Soha Jin <soha@lohu.info>
2019-06-10 18:27:41 +0800

    add static files

--- build.rs
+++ build.rs
@@ -5,23 +5,28 @@ use std::{
 	path::PathBuf
 };
 
-fn compile_templates(src: String, out: String) -> Result<()> {
+fn compile_templates(src: String, statics: String, out: String) -> Result<()> {
 	fs::create_dir_all(&src).unwrap();
+	fs::create_dir_all(&statics).unwrap();
 	fs::create_dir_all(&out).unwrap();
 	let src = PathBuf::from(src);
+	let statics = PathBuf::from(statics);
 	let out = PathBuf::from(out);
 
 	let mut ructe_compiler = Ructe::new(out)?;
+	ructe_compiler.statics()?.add_files(&statics)?;
 	ructe_compiler.compile_templates(&src)
 }
 
 fn main() -> Result<()> {
 	compile_templates(
 		format!("{}/templates-system", env::var("CARGO_MANIFEST_DIR").unwrap()),
+		format!("{}/statics-system", env::var("CARGO_MANIFEST_DIR").unwrap()),
 		format!("{}/templates-system", env::var("OUT_DIR").unwrap()),
 	)?;
 	compile_templates(
 		format!("{}/templates-theme", env::var("CARGO_MANIFEST_DIR").unwrap()),
+		format!("{}/statics-theme", env::var("CARGO_MANIFEST_DIR").unwrap()),
 		format!("{}/templates-theme", env::var("OUT_DIR").unwrap()),
 	)?;
 	Ok(())
